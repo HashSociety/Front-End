e4840201e598d500f4f5ebd2383e5ed4bacd73cf
Jayash Tripathy <jayashtripathy371@gmail.com>
2023-07-30 16:46:31 +0530

    initialized with the new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,15 @@ export default {
   theme: {
     extend: {
       colors:{
-        "background": "#0F121B",
-        "primary": "#437EFF",
-        "content-bg": "#010613",
+        "primary": "#070707" ,//primary background color
+
+        "secondary": "#2E59BF", //secondary background color , eg for buttons
+        "primaryFont": "#FFFFFF" ,//primary font color
       }
     },
     
   }, 
-  plugins: [],
+  plugins: [
+    require('tailwindcss-dotted-background'),
+  ],
 }
